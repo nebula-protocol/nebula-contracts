d83892573cc868212d4e5a2ef856e3747d3b8b95
Sam Masling <sam.masling@gmail.com>
2021-04-18 12:11:33 -0700

    add slippage prevention for burn

--- basket-scripts/basket.py
+++ basket-scripts/basket.py
@@ -46,8 +46,8 @@ class Basket:
         return {"stage_asset": {}}
 
     @staticmethod
-    def burn(asset_weights=None):
-        return {"burn": {"asset_weights": asset_weights}}
+    def burn(asset_weights=None, redeem_mins=None):
+        return {"burn": {"asset_weights": asset_weights, "redeem_mins": redeem_mins}}
 
     @staticmethod
     def reset_target(new_assets, new_target):
