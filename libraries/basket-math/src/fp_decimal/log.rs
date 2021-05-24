b448b04b8af3d6cad4b8358b78fafb42bf9fefc0
Tiger Sun <tgsun@stanford.edu>
2021-05-24 02:00:20 -0700

    think fixed ln(0.5)

--- libraries/basket-math/src/fp_decimal/log.rs
+++ libraries/basket-math/src/fp_decimal/log.rs
@@ -6,42 +6,43 @@ impl FPDecimal {
     pub fn _ln(a: FPDecimal) -> FPDecimal {
         assert!(a.sign != 0);
         let mut v = a.num;
-        let mut r = FPDecimal::zero().num;
+        let mut r = FPDecimal::zero();
         while v <= FPDecimal::ONE.num / U256([10, 0, 0, 0]) {
             v = v * U256([10, 0, 0, 0]);
-            r = r - FPDecimal::LN_10.num;
+            r = r - FPDecimal::LN_10;
         }
         while v >= U256([10, 0, 0, 0]) * FPDecimal::ONE.num {
             v = v / U256([10, 0, 0, 0]);
-            r = r + FPDecimal::LN_10.num;
+            r = r + FPDecimal::LN_10;
         }
         while v < FPDecimal::ONE.num {
             v = FPDecimal::_mul(FPDecimal { num: v, sign: 1 }, FPDecimal::E).num;
-            r = r - FPDecimal::ONE.num;
+            r = r - FPDecimal::ONE;
         }
         while v > FPDecimal::E.num {
             v = FPDecimal::_div(FPDecimal { num: v, sign: 1 }, FPDecimal::E).num;
-            r = r + FPDecimal::ONE.num;
+            r = r + FPDecimal::ONE;
         }
         if v == FPDecimal::ONE.num {
-            return FPDecimal { num: r, sign: 1 };
+            return r;
+            // return FPDecimal { num: r, sign: 1 };
         }
         if v == FPDecimal::E.num {
-            return FPDecimal {
-                num: FPDecimal::ONE.num + r,
-                sign: 1,
-            };
+            return r + FPDecimal::ONE;
+            // return FPDecimal {
+            //     num: FPDecimal::ONE.num + r,
+            //     sign: 1,
+            // };
         }
 
         let FRAC_1_5_FPDEC = FPDecimal { num: U256([3, 0, 0, 0]) * FPDecimal::ONE.num / U256([2, 0, 0, 0]), sign: 1 };
         let v = FPDecimal { num: v, sign: 1 } - FRAC_1_5_FPDEC;
 
-        r = r + FPDecimal::LN_1_5.num;
+        r = r + FPDecimal::LN_1_5;
 
         let mut m = FPDecimal::ONE * v / (v + FPDecimal { num: U256([3, 0, 0, 0]) * FPDecimal::ONE.num, sign: 1 });
         // println!("figure out why this is weird {}", (FPDecimal { num: U256([3, 0, 0, 0]), sign: 1 }) * FPDecimal::ONE);
         
-        let mut r = FPDecimal { num: r, sign: 1 };
         r = r + FPDecimal { num: U256([2, 0, 0, 0]) * FPDecimal::ONE.num, sign: 1 } * m;
         let m2 = m * m / FPDecimal::ONE;
         let mut i: u64 = 3;
@@ -55,7 +56,6 @@ impl FPDecimal {
             }
         }
         r
-        // FPDecimal { num: r, sign: 1 }
     }
 
     pub fn ln(&self) -> FPDecimal {
@@ -73,9 +73,9 @@ mod tests {
     #[test]
     fn test_ln_sanity() {
         let half = FPDecimal::one().div(2i128);
-        // println!("{}", FPDecimal::_ln(half)); // works if you comment this out
-        let num = FPDecimal::one().mul(5).div(4);
-        println!("{}", FPDecimal::_pow(num, half));
+        println!("{}", FPDecimal::_ln(half)); // works if you comment this out
+        // let num = FPDecimal::one().mul(5).div(4);
+        // println!("{}", FPDecimal::_pow(num, half));
     }
 
     #[test]
