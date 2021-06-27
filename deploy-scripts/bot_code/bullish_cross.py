5acfa309a4c08d05c0c58f1076d042e08496cb1b
Tiger Sun <tgsun@stanford.edu>
2021-06-27 02:34:51 -0400

    wip: finish up / understand bullish-cross self optimization code

--- deploy-scripts/bot_code/bullish_cross.py
+++ deploy-scripts/bot_code/bullish_cross.py
@@ -25,18 +25,37 @@ indication=highestarray(pwpsmooth); // winning percentage (smoothed)
 import pandas as pd
 
 class BullishCrossRecomposer:
-    def __init__(self, asset_names, use_test_data=False):
+    def __init__(self, asset_names, use_test_data=False, minperiod=5, maxperiod=200):
         self.use_test_data = use_test_data
-        self.lookback = 200
+        self.minperiod=minperiod, 
+        self.maxperiod=maxperiod,
         self.asset_names = asset_names
 
         # Use test data from Yahoo Finance
         if self.use_test_data:
-            self.count = 0
+            self.count = 5
             self.closes = {a: list(pd.read_csv(a + '.csv')['Close']) for a in asset_names}
+
+    def self_opt_ma(data):
+        """
+        The sample implementation (code at the end of the article) will calculate all moving 
+        averages within a given parameter range (eg. 5 bars to 200 bars), calculate the winning 
+        percentage (rising bars) on the next bar, and then pick the best performing period length.
+        """
+
+        close = self.closes[self.count:self.count + self.maxperiod]
+        c1 = self.closes[self.count-1:self.count - 1 + self.maxperiod]
+        c3 = self.closes[self.count-3:self.count - 3 + self.maxperiod]
+
 
 
     # background contracts needed to create cluster contracts
     async def recompose(self, asset_names):
         self.count += 1
+
+        self_optimized = [self_opt_ma(data) for data in self.closes]
+        self_optimized = zip(self.asset_names, self_optimized)
+
+        # pull MC data here to weight too (?)
+
         return ['mFB', 'mTSLA', 'mGOOGL'], [20, 20, 60]
