a829a54cf6518ef06b9ba3a8cd4c7c89100fbebe
Manav Aggarwal <manavaggarwal1234@gmail.com>
2021-06-29 04:01:48 -0400

    Update recomposition_Bot

--- deploy-scripts/recomposition_bot.py
+++ deploy-scripts/recomposition_bot.py
@@ -1,11 +1,21 @@
+from bot_code.bullish_cross import BullishCrossRecomposer
+from api import Asset
+
 class RecompositionBot:
-    def __init__(self, bot_name):
+    def __init__(self, bot_name, asset_names, ecosystem):
+        self.ecosystem = ecosystem
         if bot_name == 'bullish-cross':
-            self.recomposer = bullish_cross_recomposer
+            self.recomposer = BullishCrossRecomposer(asset_names, use_test_data=True)
         else:
             raise NotImplementedError
 
 
-    # background contracts needed to create cluster contracts
     async def recompose(self, asset_names):
-        return self.recomposer.recompose(asset_names, testing=True)
+        new_assets, new_target = await self.recomposer.recompose()
+
+        await self.ecosystem.cluster.reset_target(
+            assets=[Asset.cw20_asset_info(a) for a in new_assets],
+            target=new_target
+        )
+
+        return new_assets, new_target
