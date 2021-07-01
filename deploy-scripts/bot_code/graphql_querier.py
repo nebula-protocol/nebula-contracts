050a635f98c158b27d87e7bff05362d2afe7e158
Tiger Sun <tgsun@stanford.edu>
2021-07-01 03:26:03 -0400

    wip: fix mirror bugs

--- deploy-scripts/bot_code/graphql_querier.py
+++ deploy-scripts/bot_code/graphql_querier.py
@@ -32,3 +32,31 @@ async def mirror_history_query(address, tick, from_stamp, to_stamp):
     closes = [p['price'] for p in prices]
 
     return symbol, latest_timestamp, closes, mcap
+
+async def get_all_mirror_assets():
+    query = """
+    query {{
+        assets {{
+            token,
+            name,
+            symbol
+        }}
+    }}"""
+
+    # TODO: Figure out why this isn't working
+
+    headers = {'Accept-Encoding': 'gzip, deflate, br',
+               'Content-Type': 'application/json',
+               'Accept': 'application/json',
+               'Connection': 'keep-alive',
+               'DNT': '1',
+               'Origin': 'https://graph.mirror.finance',
+               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+    url = 'https://graph.mirror.finance/graphql'
+    r = requests.post(url, json={'query': query}, headers=headers)
+    r.raise_for_status()
+    assets = json.loads(r.text)['data']['assets']
+
+    addresses = [a['token'] for a in assets]
+
+    return addresses
