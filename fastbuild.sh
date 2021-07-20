921e4b8d67ff1a468706faa3eb548a3c8889c855
Alwin Peng <apeng@jumptrading.com>
2021-07-20 19:14:06 -0400

    add ./fastbuild.sh which is not great but is at least ~2x faster than ./build.sh

---
+++ fastbuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run --rm -v "$(pwd)":/code \
+  -v "$(pwd)/optimize_workspace.py":/usr/local/bin/optimize_workspace.py \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/workspace-optimizer:0.10.7
