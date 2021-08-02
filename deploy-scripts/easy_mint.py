61c2f35e366e592ec55cb448f2fb390e10e46ece
Tiger Sun <tgsun@.stanford.edu>
2021-08-01 23:33:25 -0400

    wip rebalancing bot

--- deploy-scripts/easy_mint.py
+++ deploy-scripts/easy_mint.py
@@ -16,76 +16,91 @@ import numpy as np
 cluster_contract = Contract(sys.argv[1])
 uusd_amount = int(sys.argv[2])
 
-def find_optimal_allocation(cluster_sim: ClusterSimulatorWithPenalty, ts_sims: List[TerraswapPoolSimulation], uusd_amt):
-    order_uusd = int(np.log10(uusd_amt)) - 6
-    num_chunks = max(pow(10, order_uusd), 100)
-    uusd_per_chunk = uusd_amt / num_chunks
+class EasyMintOptimizer:
+    def __init__(self, cluster_contract) -> None:
+        self.cluster_contract = cluster_contract
+        self.cluster_simulator = ClusterSimulatorWithPenalty(cluster_contract)
 
-    optimal_asset_allocation = [0 for i in range(len(cluster_sim.base_inv))]
-    uusd_per_asset = [0 for i in range(len(cluster_sim.base_inv))]
-    expected_cluster_tokens = 0
-    
-    print("Iterating through", num_chunks)
+    async def reset_initial_state(self):
+        await self.cluster_simulator.reset_initial_state()
+        target = self.cluster_simulator.cluster_state['target']
+        print("Current state", self.cluster_simulator.cluster_state)
+        prices = [float(p) for p in self.cluster_simulator.cluster_state['prices']]
+        self.pair_contracts = [Contract(await get_pair_contract_uusd(t, price=prices[idx])) for idx, t in enumerate(target)]
+        print("Pair contracts", self.pair_contracts)
 
-    for i in range(num_chunks):
-        # Greedily pick the asset that maximizes number of minted cluster tokens
-        max_asset_index, max_cluster_amt, best_asset_amt = -1, 0, 0
-        for j in range(len(cluster_sim.target_assets)):
-            asset = cluster_sim.target_assets[j]
-            ts_sim = ts_sims[j]
+        self.terraswap_simulators = [TerraswapPoolSimulation(p) for p in self.pair_contracts]
+        for ts in self.terraswap_simulators:
+            await ts.reset()
 
+    def find_optimal_allocation(self, uusd_amt):
+        """
+        Makes stateful changes to the simulators
+        """
+        ts_sims = self.terraswap_simulators
+        cluster_sim = self.cluster_simulator
+        order_uusd = int(np.log10(uusd_amt)) - 6
+        num_chunks = max(pow(10, order_uusd), 100)
+        uusd_per_chunk = uusd_amt / num_chunks
+
+        optimal_asset_allocation = [0 for i in range(len(cluster_sim.base_inv))]
+        uusd_per_asset = [0 for i in range(len(cluster_sim.base_inv))]
+        expected_cluster_tokens = 0
+        
+        print("Iterating through", num_chunks)
+
+        for i in range(num_chunks):
+            # Greedily pick the asset that maximizes number of minted cluster tokens
+            max_asset_index, max_cluster_amt, best_asset_amt = -1, 0, 0
+            for j in range(len(cluster_sim.target_assets)):
+                asset = cluster_sim.target_assets[j]
+                ts_sim = ts_sims[j]
+
+                offer_asset = Asset.asset('uusd', uusd_per_chunk, native=True)
+
+                asset_amt, _, _ = ts_sim.simulate_swap(offer_asset=offer_asset)
+
+                add_amts = [0 for _ in range(len(cluster_sim.target_assets))]
+                add_amts[j] = asset_amt
+
+                cluster_token_amt = cluster_sim.simulate_mint(add_amts)
+
+                if cluster_token_amt > max_cluster_amt:
+                    max_cluster_amt = cluster_token_amt
+                    max_asset_index = j
+                    best_asset_amt = asset_amt
+
+            assert max_asset_index != -1
+
+            # Make choice here
+            optimal_asset_allocation[max_asset_index] += best_asset_amt
+            uusd_per_asset[max_asset_index] += uusd_per_chunk
+
+            expected_cluster_tokens += max_cluster_amt
+
+            # Update terraswap sim with trade
             offer_asset = Asset.asset('uusd', uusd_per_chunk, native=True)
+            ts_sims[max_asset_index].execute_swap(offer_asset=offer_asset)
 
-            asset_amt, _, _ = ts_sim.simulate_swap(offer_asset=offer_asset)
-
+            # Update cluster sim with mint
             add_amts = [0 for _ in range(len(cluster_sim.target_assets))]
-            add_amts[j] = asset_amt
+            add_amts[max_asset_index] = best_asset_amt
+            cluster_sim.execute_mint(add_amts)
 
-            cluster_token_amt = cluster_sim.simulate_mint(add_amts)
-
-            if cluster_token_amt > max_cluster_amt:
-                max_cluster_amt = cluster_token_amt
-                max_asset_index = j
-                best_asset_amt = asset_amt
-
-        assert max_asset_index != -1
-
-        # Make choice here
-        optimal_asset_allocation[max_asset_index] += best_asset_amt
-        uusd_per_asset[max_asset_index] += uusd_per_chunk
-
-        expected_cluster_tokens += max_cluster_amt
-
-        # Update terraswap sim with trade
-        offer_asset = Asset.asset('uusd', uusd_per_chunk, native=True)
-        ts_sims[max_asset_index].execute_swap(offer_asset=offer_asset)
-
-        # Update cluster sim with mint
-        add_amts = [0 for _ in range(len(cluster_sim.target_assets))]
-        add_amts[max_asset_index] = best_asset_amt
-        cluster_sim.execute_mint(add_amts)
-
-    return optimal_asset_allocation, uusd_per_asset, expected_cluster_tokens
+        return optimal_asset_allocation, uusd_per_asset, expected_cluster_tokens
 
 
 async def main():
-    cluster_simulator = ClusterSimulatorWithPenalty(cluster_contract)
-    await cluster_simulator.set_initial_state()
-    target = cluster_simulator.cluster_state['target']
-    print("Current state", cluster_simulator.cluster_state)
-    prices = [float(p) for p in cluster_simulator.cluster_state['prices']]
-    pair_contracts = [Contract(await get_pair_contract_uusd(t, price=prices[idx])) for idx, t in enumerate(target)]
-    print(pair_contracts)
-
-    terraswap_simulators = [TerraswapPoolSimulation(p) for p in pair_contracts]
-    for ts in terraswap_simulators:
-        await ts.reset()
-
-
+    easy_minter = EasyMintOptimizer(cluster_contract)
+    await easy_minter.reset_initial_state()
+    cluster_simulator = easy_minter.cluster_simulator
+    terraswap_simulators = easy_minter.terraswap_simulators
     print("Base inv before algo", cluster_simulator.base_inv)
     print("Imbalance before algo", cluster_simulator.get_curr_imbalance())
 
-    opt_allocs, opt_uusd_spends, expected_ct_amount = find_optimal_allocation(cluster_simulator, terraswap_simulators, uusd_amount)
+
+    print("did this work")
+    opt_allocs, opt_uusd_spends, expected_ct_amount = easy_minter.find_optimal_allocation(uusd_amount)
     print(f'Opt asset alloc: {opt_allocs}, opt uusd spend: {opt_uusd_spends}, expected amount: {expected_ct_amount}')
 
     print("Base inv after algo", cluster_simulator.base_inv)
