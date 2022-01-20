b0c5b23581e18ea6d486df5b69311e9e95e62549
traviolus <tumcharoen@terra.money>
2022-01-20 13:31:17 +0700

    impv: implement tests for nebula-oracle

---
+++ contracts/nebula-oracle/src/testing/mock_querier.rs
@@ -0,0 +1,101 @@
+use cosmwasm_std::testing::{MockApi, MockQuerier, MockStorage, MOCK_CONTRACT_ADDR};
+use cosmwasm_std::{
+    from_binary, from_slice, to_binary, Coin, ContractResult, Decimal, OwnedDeps, Querier,
+    QuerierResult, QueryRequest, SystemError, SystemResult, WasmQuery,
+};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use tefi_oracle::hub::PriceResponse;
+use terra_cosmwasm::{ExchangeRateItem, ExchangeRatesResponse, TerraQuery, TerraQueryWrapper, TerraRoute};
+
+/// mock_dependencies is a drop-in replacement for cosmwasm_std::testing::mock_dependencies
+/// this uses our CustomQuerier.
+pub fn mock_dependencies(
+    contract_balance: &[Coin],
+) -> OwnedDeps<MockStorage, MockApi, WasmMockQuerier> {
+    let contract_addr = MOCK_CONTRACT_ADDR.to_string();
+    let custom_querier: WasmMockQuerier =
+        WasmMockQuerier::new(MockQuerier::new(&[(&contract_addr, contract_balance)]));
+
+    OwnedDeps {
+        storage: MockStorage::default(),
+        api: MockApi::default(),
+        querier: custom_querier,
+    }
+}
+
+pub struct WasmMockQuerier {
+    base: MockQuerier<TerraQueryWrapper>,
+}
+
+impl Querier for WasmMockQuerier {
+    fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
+        // MockQuerier doesn't support Custom, so we ignore it completely here
+        let request: QueryRequest<TerraQueryWrapper> = match from_slice(bin_request) {
+            Ok(v) => v,
+            Err(e) => {
+                return SystemResult::Err(SystemError::InvalidRequest {
+                    error: format!("Parsing query request: {:?}", e),
+                    request: bin_request.into(),
+                })
+            }
+        };
+        self.execute_query(&request)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Price {
+        asset_token: String,
+        timeframe: Option<u64>,
+    }
+}
+
+impl WasmMockQuerier {
+    pub fn execute_query(&self, request: &QueryRequest<TerraQueryWrapper>) -> QuerierResult {
+        match &request {
+            QueryRequest::Custom(TerraQueryWrapper { route, query_data }) => {
+                if route == &TerraRoute::Oracle {
+                    match query_data {
+                        TerraQuery::ExchangeRates { base_denom, quote_denoms } => {
+                            let res = ExchangeRatesResponse {
+                                base_denom: base_denom.to_string(),
+                                exchange_rates: quote_denoms.iter().map(|x| {
+                                    ExchangeRateItem {
+                                        quote_denom: x.clone(),
+                                        exchange_rate: Decimal::one(),
+                                    }
+                                }).collect::<Vec<_>>()
+                            };
+                            SystemResult::Ok(ContractResult::from(to_binary(&res)))
+                        }
+                        _ => panic!("DO NOT ENTER HERE"),
+                    }
+                } else {
+                    panic!("DO NOT ENTER HERE")
+                }
+            }
+            QueryRequest::Wasm(WasmQuery::Smart { contract_addr: _, msg }) => match from_binary(&msg)
+                .unwrap()
+            {
+                QueryMsg::Price { .. } => {
+                    SystemResult::Ok(ContractResult::from(to_binary(&PriceResponse {
+                        rate: Decimal::one(),
+                        last_updated: u64::MAX
+                    })))
+                }
+            },
+            _ => self.base.handle_query(request),
+        }
+    }
+}
+
+impl WasmMockQuerier {
+    pub fn new(base: MockQuerier<TerraQueryWrapper>) -> Self {
+        WasmMockQuerier {
+            base,
+        }
+    }
+}
--- contracts/nebula-oracle/src/testing/tests.rs
+++ contracts/nebula-oracle/src/testing/tests.rs
@@ -1,18 +1,24 @@
-use cosmwasm_std::Addr;
-use crate::contract::instantiate;
+use crate::contract::{execute, instantiate, query};
 use crate::state::{Config, read_config};
-use cosmwasm_std::testing::{mock_dependencies, mock_info, mock_env};
-use nebula_protocol::oracle::InstantiateMsg;
+use crate::testing::mock_querier::mock_dependencies;
+use astroport::asset::AssetInfo;
+use cosmwasm_std::{Addr, Decimal, from_binary, StdError};
+use cosmwasm_std::testing::{mock_info, mock_env};
+use nebula_protocol::oracle::{ExecuteMsg, InstantiateMsg, PriceResponse, QueryMsg};
+
+fn init_msg() -> InstantiateMsg {
+    InstantiateMsg {
+        owner: "owner0000".to_string(),
+        oracle_addr: "oracle0000".to_string(),
+        base_denom: "uusd".to_string()
+    }
+}
 
 #[test]
 fn proper_initialization() {
     let mut deps = mock_dependencies(&[]);
     let info = mock_info("sender0000", &[]);
-    let msg = InstantiateMsg {
-        owner: "owner0000".to_string(),
-        oracle_addr: "oracle0000".to_string(),
-        base_denom: "uusd".to_string()
-    };
+    let msg = init_msg();
     let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
     assert_eq!(0, res.messages.len());
 
@@ -25,4 +31,58 @@ fn proper_initialization() {
             base_denom: "uusd".to_string(),
         }
     );
+}
+
+#[test]
+fn update_config() {
+    let mut deps = mock_dependencies(&[]);
+    let info = mock_info("sender0000", &[]);
+    let msg = init_msg();
+    let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+    assert_eq!(0, res.messages.len());
+
+    // unauthorized update
+    let info = mock_info("imposter0000", &[]);
+    let msg = ExecuteMsg::UpdateConfig {
+        owner: Some("imposter0000".to_string()),
+        oracle_addr: Some("oracle0001".to_string()),
+        base_denom: None,
+    };
+    let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap_err();
+    assert_eq!(res, StdError::generic_err("unauthorized"));
+
+    // successful update
+    let info = mock_info("owner0000", &[]);
+    let msg = ExecuteMsg::UpdateConfig {
+        owner: Some("owner0001".to_string()),
+        oracle_addr: Some("oracle0001".to_string()),
+        base_denom: Some("uusd".to_string()),
+    };
+    let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+    let config = read_config(&deps.storage).unwrap();
+    assert_eq!(
+        config,
+        Config {
+            owner: Addr::unchecked("owner0001"),
+            oracle_addr: Addr::unchecked("oracle0001"),
+            base_denom: "uusd".to_string()
+        }
+    )
+}
+
+#[test]
+fn query_price() {
+    let mut deps = mock_dependencies(&[]);
+    let info = mock_info("sender0000", &[]);
+    let msg = init_msg();
+    let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+    assert_eq!(0, res.messages.len());
+
+    let msg = QueryMsg::Price {
+        base_asset: AssetInfo::Token { contract_addr: Addr::unchecked("contractaddress0001") },
+        quote_asset: AssetInfo::NativeToken { denom: "uusd".to_string() },
+    };
+    let res = query(deps.as_ref(), mock_env(), msg).unwrap();
+    let price: PriceResponse = from_binary(&res).unwrap();
+    assert_eq!(price.rate, Decimal::one());
 }
