a1461d98eefcdab5a3543b139325f0272c7d0b68
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2022-03-15 17:50:16 +0700

    chore: remove unused files

--- fastbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v "$(pwd)":/code \
-  -v "$(pwd)/optimize_workspace.py":/usr/local/bin/optimize_workspace.py \
-  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.11.5
